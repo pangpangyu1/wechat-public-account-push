77cd772386a2d7788831ba184218896543f27fb2
pangpangyu1 <100704789+pangpangyu1@users.noreply.github.com>
2022-10-08 14:21:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,20 +23,17 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ow_9-6Qv6gpGAwORGUpAvovgPtew',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'i5e0x2pf1YpjwEa8pyymabJJPtcYFc_jW2x6_qxAQTk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-21',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '2001', date: '06-21',
+          type: '生日', name: '宝贝', year: '2001', date: '06-21',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2019', date: '11-11',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
