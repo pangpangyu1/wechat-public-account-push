65dda0b6e9d854a8137a3ee07d7ff0ad7496bd70
pangpangyu1 <100704789+pangpangyu1@users.noreply.github.com>
2022-10-08 14:47:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ow_9-6Qv6gpGAwORGUpAvovgPtew',
+      id: 'ow_9-6RwQPoNgrFnwZVUmVicacAQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'i5e0x2pf1YpjwEa8pyymabJJPtcYFc_jW2x6_qxAQTk',
+      useTemplateId: 'GqCmSZHmWreLkXfyByPeHaId3yFmijk96K1G7AVjxHU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-21',
       festivals: [
